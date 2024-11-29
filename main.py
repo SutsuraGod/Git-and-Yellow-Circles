ef5d62a806134f707f489d9106e8490dcb964843
Sutsura <sutsura0123@gmail.com>
2024-11-29 17:23:30 +0300

    Second commit

--- main.py
+++ main.py
@@ -5,61 +5,13 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import Qt
+from mainUi import Ui_MainWindow
 
 
-template = '''<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>463</width>
-    <height>472</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="createCircle">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>10</y>
-      <width>461</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Создать окружность</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>463</width>
-     <height>21</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-'''
-
-
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
 
         self.do_paint = False
         self.createCircle.clicked.connect(self.paint)
@@ -77,7 +29,7 @@ class MainWindow(QMainWindow):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
         qp.setPen(Qt.PenStyle.NoPen)
 
         diameter = randint(0, 400)
